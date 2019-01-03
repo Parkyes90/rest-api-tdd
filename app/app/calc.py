7dc7d8e3b66218553252f5158c3b6f020062acfa
parkyes90 <parkyes90@gmail.com>
2019-01-03 18:58:21 +0900

    add test

---
+++ app/app/calc.py
@@ -0,0 +1,18 @@
+def add(x: int, y: int) -> int:
+    """
+    add two number together
+    :param x: int
+    :param y: int
+    :return: int
+    """
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """
+    subtract x from y and return value
+    :param x: int
+    :param y: int
+    :return: int
+    """
+    return y - x
